7385fffecde9ed8004d11587db7709ece5901a4a
EliasDallis <56697876+EliasDallis@users.noreply.github.com>
2021-02-13 16:11:59 +0000

    Added new Home Page design - uses bootstrap
    Added new Home Page design - main menu/home page with icons

---
+++ Elias/WireFrame00.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">                    <!-- OPENING HTML TAG -->
+
+
+<!-------------------------------->
+<!--          HEAD              -->
+<!-------------------------------->
+
+<head>
+    <title>Page Title - in chrome tab</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!------------------------------- >         These link to the div sections in the body:
+    <--          STYLE              -->
+    <!------------------------------- >         body / .header / .header h1 / navbar / row / side / footer etc. -->
+
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    
+</head>
+
+
+<!-------------------------------->
+<!--          BODY              -->
+<!-------------------------------->
+
+<body>
+<div class="container">
+
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">HappyTech HR</a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Feedback</a></li>
+              <li><a href="#about">Templates</a></li>
+              <li><a href="#contact">Roles</a></li>
+              
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+
+    
+    </div>
+
+    <div class="container theme-showcase" role="main">
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Main Menu</h1>
+        
+      </div>
+
+      <div class="page-header">
+        <h3>Select an option</h3>
+      </div>
+    
+    <div class="container">
+        <div class="row">
+            <div style="text-align:center" class="col-md-4">
+                <a href="#">
+                <img  data-src="holder.js/200x200" src="bulletList.png" class="img-thumbnail" alt="A generic square placeholder image with a white border around it, making it resemble a photograph taken with an old instant camera">
+                <h3> Give Feedback </h3>
+                </a>
+            </div>
+            <div style="text-align:center" class="col-md-4">
+                <a href="#">
+                <img data-src="holder.js/200x200" src="template.png" class="img-thumbnail" alt="A generic square placeholder image with a white border around it, making it resemble a photograph taken with an old instant camera">
+                <h3> Select Template </h3>
+                </a>
+            </div>
+            <div style="text-align:center" class="col-md-4">
+                <a href="#">
+                <img data-src="holder.js/200x200" src="person.png" class="img-thumbnail" alt="A generic square placeholder image with a white border around it, making it resemble a photograph taken with an old instant camera">
+                <h3> Roles / Candidates </h3>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+</div>
+</body>
+
+
+
+
+
+
+</html>                             <!-- CLOSING HTML TAG -->
+
+
+
+
+
+<!-- <div class="container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md">
+                        <img data-src="holder.js/200x200" src="bulletList.png" class="img-thumbnail" alt="A generic square placeholder image with a white border around it, making it resemble a photograph taken with an old instant camera">
+                        <h3> Give Feedback </h3>
+                    </div>
+                    <div class="col-md">
+                        <img data-src="holder.js/200x200" src="bulletList.png" class="img-thumbnail" alt="A generic square placeholder image with a white border around it, making it resemble a photograph taken with an old instant camera">
+                        <h3> Select Template </h3>
+                    </div>
+                    <div class="col-md">
+                        <img data-src="holder.js/200x200" src="bulletList.png" class="img-thumbnail" alt="A generic square placeholder image with a white border around it, making it resemble a photograph taken with an old instant camera">
+                        <h3> Roles / Candidates </h3>
+                    </div>
+                </div>
+            </div>
+      
+        </div> -->
